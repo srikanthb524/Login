5985c0d354546838c6a51f33289bb027cb9223ee
srikanthb524 <srikanthb@spyhre.com>
2022-04-13 12:44:07 +0530

    EOD13-04

---
+++ src/app/emodetails/emodetails.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-emodetails',
+  templateUrl: './emodetails.component.html',
+  styleUrls: ['./emodetails.component.css']
+})
+export class EmodetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
